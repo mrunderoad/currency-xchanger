6c3953731b545cc81a02dcba83f89c7df2556b4d
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-11 09:46:57 -0800

    Update API call to use params

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,6 @@
 export default class ExchangeCurrency {
   static getExchange() {
-    return (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    return (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencies}/${amount}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.status);
